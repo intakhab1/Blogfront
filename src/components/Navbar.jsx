82f1f8f3e71877a76e37ece15b1a9fd081d90730
Mohd Intakhab Alam <amirint190@gmail.com>
2024-03-07 21:15:10 +0530

    fix common import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 					console.log(err);
 				});
 		}
-	}, [token]);
+	}, [token,]);
 
 	const handleNavPanelVisibility = () => {
 		setNavPanelVisibility((currentVal) => !currentVal);
@@ -72,7 +72,7 @@ const Navbar = () => {
 				</Link>
 				<div
 					className={
-						"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
+						"absolute bg-white w-full left-0  mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
 						(searchBoxVisibility ? "show" : "hide")
 					}
 				>
@@ -81,19 +81,20 @@ const Navbar = () => {
 						input="text"
 						onKeyDown={handleSearch}
 						placeholder="Search"
-						className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 "
+						className="w-1/2 md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 "
 					></input>
-					<i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey "></i>
+					{/* <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey "></i> */}
 				</div>
+				
 				<div className="flex items-center gap-1 md:gap-6 ml-auto">
-					<button
+					{/* <button
 						className="md:hidden w-11 h-11 rounded-full flex items-center justify-center hover:bg-black/10"
 						onClick={() => {
 							setSearchBoxVisibility((currentVal) => !currentVal);
 						}}
 					>
 						<i className="fi fi-rr-search text-xl"></i>
-					</button>
+					</button> */}
 					<Link to="/editor" className=" md:flex gap-2 rounded-full link ">
 						<i className="fi fi-rr-file-edit"></i>
 						<p className="hidden sm:block md:block lg:block xl:block">Write</p>
