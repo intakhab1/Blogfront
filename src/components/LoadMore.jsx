c8cd1803b5bc9ab8f4cdc55810c73c9e171b8d3e
Mohd Intakhab Alam <amirint190@gmail.com>
2024-03-13 00:58:56 +0530

    fix common import

--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -4,7 +4,7 @@ export const LoadMore = ({ state, fetchData, additionalParam }) => {
         return (
             <button 
                 onClick={() => fetchData({ ...additionalParam, page: state.page + 1}) }
-                className="p-2 px-3 text-dark-grey hover:bg-grey/30 rounded-md flex items-center gap-2"
+                className="latestTag p-2 px-3 text-dark-grey hover:bg-grey/30 rounded-full flex items-center gap-2"
             >
             Load More</button>
         )
